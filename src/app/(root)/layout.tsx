f92a1d08cde98154095583aea610ada08f64bbdd
Aleksandr <kartanovaleks@gmail.com>
2024-09-19 19:05:58 +0300

    Fix deploy

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Header } from "../../shared/components/shared/header";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Next Pizza | Main",
@@ -8,14 +9,16 @@ export const metadata: Metadata = {
 
 export default function HomeLayout({
   children,
-  modal
+  modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
   return (
     <main className='min-h-screen'>
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       {children}
       {modal}
     </main>
